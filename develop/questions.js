6475e07cdcdc9c16a43c1df0a8d7e6728fe21eba
Lamor Odingo <l2odingo@gmail.com>
2022-01-22 14:28:26 -0800

    added styling

--- develop/questions.js
+++ develop/questions.js
@@ -1,16 +1,8 @@
-// Start Page
-
-
-
-
-
-
-
 // User Choices
 
 var questions = [
     {
-        title: "Commonly used data types DO NOT include:",
+        title: "How many licks does it take to get to a center of a tootsie pop",
         choices: [
             "strings",
             "booleans",
@@ -65,23 +57,26 @@ let timer = document.querySelector('.time');
 
 // Timer Button 
 
-var secondsLeft = 120;
+var secondsLeft = 90;
 
 function setTimer() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
         timer.textContent = 'Time Left: ' + secondsLeft + ' seconds';
 
-        if (secondsLeft === 0) {
+        if (secondsLeft === 0 || questions.length <= currentQuestion ) {
             clearInterval(timerInterval);
-        };
+            body.textContent = '';
+            body.appendChild(quizComplete);
+            quizComplete.style.opacity = 1;
+            timeSection.textContent = '';
+            displayResults();
+        }
+
 
     }, 1000);
 
 };
-
-
-
 
 
 // Start Button
@@ -89,19 +84,21 @@ function setTimer() {
 let startButton = document.querySelector('.start-button');
 let intro = document.querySelector('.position-intro');
 let quiz = document.querySelector('.position-quiz');
-let body = document.querySelector('.style-body');
+let body = document.querySelector('.position-body');
+let bodySection = document.querySelector('.style-body');
+let header = document.querySelector('.style-header');
+let timeSection = document.querySelector('.time');
 
 
-quiz.style.display = 'none';
+
 
 startButton.addEventListener('click', function(){
-    intro.style.display = 'none';
-    quiz.style.display = 'block';
-    
+    quiz.style.opacity = 1;
+    body.textContent = '';
+    body.appendChild(quiz);
+     
     setTimer();
 });
-
-
 
 
 
@@ -130,8 +127,6 @@ function evaluateChoice(answer, choice) {
 };
 
 
-
-
 optionA.addEventListener('click', function () {
     evaluateChoice(questions[currentQuestion].answer, questions[currentQuestion].choices[0]);
 });
@@ -153,31 +148,91 @@ optionD.addEventListener('click', function() {
 
 var quizComplete = document.querySelector('.position-complete');
 
-quizComplete.style.display = 'none';
 
 function displayQuestion() {
     if (questions.length > currentQuestion) {
         quizQuestion.textContent = questions[currentQuestion].title;
-        optionA.textContent = questions[currentQuestion].choices[0];
-        optionB.textContent = questions[currentQuestion].choices[1];
-        optionC.textContent = questions[currentQuestion].choices[2];
-        optionD.textContent = questions[currentQuestion].choices[3];
+        optionA.textContent = 'A. ' + questions[currentQuestion].choices[0];
+        optionB.textContent = 'B. ' + questions[currentQuestion].choices[1];
+        optionC.textContent = 'C. ' + questions[currentQuestion].choices[2];
+        optionD.textContent = 'D. ' + questions[currentQuestion].choices[3];
     }
     else {
-        quiz.style.display = 'none';
-        quizComplete.style.display = 'block';
+        body.textContent = '';
+        quizComplete.style.opacity = 1;
+        body.appendChild(quizComplete);
         displayResults();
     }
-
+    
 }
 
 
-
 var results = document.querySelector('.results');
+var submission = document.querySelector('.submit-button');
+var inputInitial = document.querySelector('.style-input')
 
 function displayResults() {
     results.textContent = 'Your score is: ' + correct + "/5";
 }
+
+
+// High Scores Section
+
+
+var firstPlace = document.querySelector('.first');
+var highScores = document.querySelector('.position-scores');
+
+
+
+submission.addEventListener('click', function() {
+    highScores.style.opacity = 1;
+    body.textContent = '';
+    body.appendChild(highScores);
+    
+    var theInitial = {
+        inputInitial: inputInitial.value.trim(),
+        correctScores: correct
+    };
+
+    localStorage.setItem('theInitial', JSON.stringify(theInitial))
+    displayHighScores();
+});
+
+
+function displayHighScores() {
+    var initial = JSON.parse(localStorage.getItem('theInitial'));
+    if (initial !== null) {
+        firstPlace.textContent = initial.inputInitial.toUpperCase() + ' - ' + initial.correctScores + 'pts';
+    }
+};
+
+var clearButton = document.querySelector('.style-clear');
+var playAgain = document.querySelector('.style-again');
+
+clearButton.addEventListener('click', function() {
+    firstPlace.textContent = '';
+    secondPlace.textContent = '';
+    thirdPlace.textContent = '';
+
+});
+
+playAgain.addEventListener('click', function() {
+    window.location.reload() //resets the code back to the beginning
+})
+
+let viewScore = document.querySelector('.score-button');
+
+viewScore.addEventListener('click', (event) => {
+        event.preventDefault();
+        body.textContent = '';
+        body.appendChild(highScores);
+        highScores.style.opacity = 1;
+        displayHighScores();
+    });
+
+    
+
+
 
 
 
